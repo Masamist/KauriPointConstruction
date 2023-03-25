8fa51f080bae517f08cb42ff77e8e4133bb5d77d
ludo <ludobourneville@gmail.com>
2023-03-25 23:00:44 +1300

    Variables added. maping address

--- src/pages/project/projectDetail.js
+++ src/pages/project/projectDetail.js
@@ -1,13 +1,24 @@
+import { format } from "path"
 
-export default function projectDetail({project}) {
+export default function ProjectDetail({project}) {
+
+    const name = project.name
+    const startDate = project.startDate ? Object.entries(project.startDate).map( ([key,value]) => {return key + ': ' + value / 60 / 60 / 24 / 365 }) : "undefined"
+    const clientName = project.clientName
+    const phone = project.phone ? project.phone : '-'
+    const email = project.email ? project.email : '-'
+    const address = Object.entries(project.address).map( ([key,value]) => { return (key + ': ' + value + ', ') })
+    const details = project.description ? project.description : '-' 
+    const subContractFee = project.subContractFee ? project.subContractFee * 100 + '%' : '-'
+
   return (
     <div>
       <div className="project-summary">
-        <h2 className="page-title">Project: {project.name}</h2>
-        <p>Client Name: {project.client}</p>
-        <p>Phone: {project.phone}</p>
-        <p>Email: {project.email}</p>
-        <p>Address: {project.address}</p>
+        <h2 className="page-title">Project: {name}</h2>
+        <p>Client Name: {clientName}</p>
+        <p>Phone: {phone}</p>
+        <p>Email: {email}</p>
+        <p>Address: {address}</p>
 
         {/* Financial details */}
         <div>
@@ -28,11 +39,11 @@ export default function projectDetail({project}) {
         
         <h3>Project Detail</h3>
         <p className="due-date">
-          Start date: {project.startDate}
+          Start date:
         </p>
         <p>GST NO: {project.gstNo}</p>
         <p>Sub Contract Fee: {project.gstNo}%</p>
-        <p className="details">{project.discription}</p>
+        <p className="details">{details}</p>
         <p>Staff Rate:</p>
 
         {/* <h4>Staff Rate:</h4>
