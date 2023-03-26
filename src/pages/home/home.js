47d09fe0c27b570a7f934311daa56b29946702f4
ludo <ludobourneville@gmail.com>
2023-03-26 16:28:43 +1300

    Home Services

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,18 @@
 import './home.css'
+import Service_DoubleGarage from '../../assets/service_doubleGarage.png'
+import Service_Renovation from '../../assets/service_renovation.jpg'
+import Service_fencingDecking from '../../assets/service_fencingDecking.JPG'
+import Service_demolition from '../../assets/service_demolition.JPG'
+import Service_houseRemoval from '../../assets/service_houseRemoval.JPG'
+import Service_extension from '../../assets/service_extension.JPG'
+
 
 
 export default function Home() {
     return (
         <div>
             <Poster />
+            <Services />
         </div>
     )
 }
@@ -17,9 +25,67 @@ function Poster() {
                 Through our expertise.
             </h2>
             <div className='poster-text'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                tempor incididunt ut labore et dolore magna aliqua.
             </div>
         </div>
 
+    )
+}
+
+function Services () {
+    return(
+        <div className='pad'>
+            <h1>Kauri Point Construction Services</h1>
+            <div className='services'>
+                <Service 
+                    title='Construction' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_DoubleGarage}
+                />
+                <Service 
+                    title='Renovation' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_Renovation}
+
+                />
+                <Service 
+                    title='Extension' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_extension}
+                />
+                <Service 
+                    title='Fencing and Decking' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_fencingDecking}
+                />
+                <Service 
+                    title='Demolition' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_demolition}
+                />
+                <Service 
+                    title='House Removal' 
+                    text='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+                        tempor incididunt ut labore et dolore magna aliqua.'
+                    imageURL={Service_houseRemoval}
+                />
+                </div>
+        </div>
+    )
+}
+
+function Service({ title, text, imageURL }) {
+    return (
+        <div className='service'>
+            <img className='service-image' src={imageURL} alt='service'/>
+            <h2>{title}</h2>
+            <p>{text}</p>
+        </div>
     )
 }
