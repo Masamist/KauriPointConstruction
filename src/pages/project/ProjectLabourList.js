1158494c0e2f707bebc359586f8364cad8693d52
Masamist <msm-k0304@hotmail.com>
2023-03-28 21:26:05 +1300

    add Labour list

--- src/pages/project/ProjectLabourList.js
+++ src/pages/project/ProjectLabourList.js
@@ -1,26 +1,34 @@
+import { useState } from 'react'
+
 // styles
 import './ProjectLabourList.css'
 
 
 export default function ProjectLabourList({project}) {
 
+  const [totalDays, setTotalDays] = useState([])
+
 
   return (
     <div className="project-detail">
         <h3>Labour Const Breakdown by Stage</h3>
-        <LabourList labourList={project.labourList} />
-        
+        <div className="project-labour-list">
+          <LabourList labourList={project.labourList} />
+        </div>    
     </div>
   )
 }
 
 function LabourList({ labourList }) {
+
   return (
-    <div className="project-labour-list">
+    <div>
       {Object.entries(labourList).map( ([key, groups]) => { return ( 
         <div key={key} className="task-group-container">
-          <strong>{key}</strong>
+          <div className="task-group-name-container"><strong>{key}</strong></div>
           <LabourListTask groups={groups}/>
+          <div><p>Total Days</p></div>
+          <div><p>Total Amount</p></div>
         </div> 
     )})}
     </div>
@@ -31,11 +39,34 @@ function LabourListTask( {groups} ) {
   return (
     <>
       {Object.entries(groups).map( ([key, groups]) => {
-        const name = groups.name
         return (
           <div key={key}>
-            <span>{name}</span>
+            <div>
+              <span>{groups.name}</span>
+            </div>
+            <HoursPredicted hoursPredicted={groups.hoursPredicted} />
+            
           </div>
+        )
+      })}
+    </>
+  )
+}
+
+function HoursPredicted( {hoursPredicted} ) {
+  // const total = {}
+  
+
+  return (
+    <>
+      {Object.entries(hoursPredicted).map( ([key, hours]) => {
+        if(hours===""){
+          hours = "-"
+        }
+        return (
+          <>
+            <span key={key}> {hours} </span>
+          </>
         )
       })}
     </>
