4abd0a340019b013c198b80a71b90e93aeec5fe6
ludo <ludobourneville@gmail.com>
2023-04-01 17:03:22 +1300

    completed Projects

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,10 +1,15 @@
 import './Projects.css'
 import FrontDoor from '../../assets/projects/FrontDoor.JPG'
 
+import bathroomLedMirror from '../../assets/projects/example_bathroomLedMirror.JPG'
+import doubleGarage from '../../assets/projects/example_doubleGarage.png'
+import benchTop from '../../assets/projects/example_benchtop.jpg'
+
 export default function Projects() {
     return (
         <>
             <ShowcasePoster />
+            <CompletedProjects />
         </>
     )
 }
@@ -23,5 +28,56 @@ function ShowcasePoster() {
             </div>
             <img className='showcaseImage' src={FrontDoor} alt='Front Door'></img>
         </div>
+    )
+}
+function CompletedProjects(){
+    return(
+        <div className='completedProjects'>
+            <h1>Our Completed Projects</h1>
+            <Project 
+                imageUrl={benchTop} 
+                title='Kitchen Renovation' 
+                subTitle='Sarah & Harry House in 2020'
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna 
+                liqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.' 
+            />
+            <Project 
+                imageUrl={doubleGarage} 
+                title='Shed Construction' 
+                subTitle='Sarah & Harry House in 2020'
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna 
+                liqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.' 
+            />
+            <Project 
+                imageUrl={bathroomLedMirror} 
+                title='Bathroom Renovation' 
+                subTitle='Sarah & Harry House in 2020'
+                text='Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt ut labore et dolore magna 
+                liqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                ullamco laboris nisi ut aliquip ex ea commodo consequat.' 
+            />
+        </div>
+    )
+}
+
+function Project ({imageUrl, title, subTitle, text}) {
+    return (
+        <>
+            <div className='projectCard'>
+                <div  className='text'>
+                    <h2>{title}</h2>
+                    <p>{subTitle}</p>
+                    <p>
+                    {text}
+                    </p>
+                </div>
+                <img src={imageUrl} alt='previous work example'></img>
+            </div>
+        </>
     )
 }
