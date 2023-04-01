64fa5d37461da5a0b4db0563fba331a5c3da0a97
ludo <ludobourneville@gmail.com>
2023-04-01 14:18:14 +1300

    showcase Poster

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -1,7 +1,27 @@
 import './Projects.css'
+import FrontDoor from '../../assets/projects/FrontDoor.JPG'
 
 export default function Projects() {
     return (
-        <h1>PROJECTS</h1>
+        <>
+            <ShowcasePoster />
+        </>
+    )
+}
+
+function ShowcasePoster() {
+    return (
+        <div className='showcasePoster'>
+            <div className='text'>
+                <h1>Showcasing our creativity and Excellence</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna 
+                    liqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+            </div>
+            <img className='showcaseImage' src={FrontDoor} alt='Front Door'></img>
+        </div>
     )
 }
