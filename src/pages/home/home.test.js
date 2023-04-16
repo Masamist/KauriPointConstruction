baa38fb2df0c674c384f6b7e8401e3e9e2082306
ludo <ludobourneville@gmail.com>
2023-04-16 14:00:29 +1200

    more unit tests

--- src/pages/home/home.test.js
+++ src/pages/home/home.test.js
@@ -4,15 +4,39 @@ import '@testing-library/jest-dom'
 import Home from './home';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
-//UNIT TESTS Check Rendering
+//UNIT TESTS
+
+//GET BY
 test('testing app home heading', () => {
   render(<BrowserRouter><Home /></BrowserRouter>);
   const headerElement = screen.getByText(/Kauri Point Construction Services/i);
   expect(headerElement).toBeInTheDocument();
 })
 
-test('testing New builds section appears on home', () => {
+test('testing New builds heading appears on home', () => {
   render(<BrowserRouter><Home /></BrowserRouter>);
-  const newBuildsSection = screen.getByText(/New builds/i);
+  const newBuildsSection = screen.getByRole("heading", {name: 'New builds'});
   expect(newBuildsSection).toBeInTheDocument();
+})
+test('testing Exterior heading section appears on home', () => {
+  render(<BrowserRouter><Home /></BrowserRouter>);
+  const newBuildsSection = screen.getByRole("heading", {name: 'Exterior'});
+  expect(newBuildsSection).toBeInTheDocument();
+})
+
+//FIND BY
+test('paragraph on home section New Builds', async() => {
+  render(<BrowserRouter><Home /></BrowserRouter>);
+  const newBuildsparagraph = await screen.findByText(/Across our team, we have decades of building project experience/i);
+  expect(newBuildsparagraph).toBeInTheDocument();
+})
+test('section on home page: testimonial ', async() => {
+  render(<BrowserRouter><Home /></BrowserRouter>);
+  const testimonialText = await screen.findByText(/We hired Kauri Point in 2022 to renovate our Ponsonby Villa/i);
+  expect(testimonialText).toBeInTheDocument();
+})
+test('section on home page: support local communities', async() => {
+  render(<BrowserRouter><Home /></BrowserRouter>);
+  const testimonialText = await screen.findByText(/Supporting local communitites/i);
+  expect(testimonialText).toBeInTheDocument();
 })
