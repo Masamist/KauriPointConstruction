361824c5d8fafb7ca6f4ab3b4835e235538510c5
ludo <ludobourneville@gmail.com>
2023-04-17 11:01:36 +1200

    ProjectDetail-Client info
    introduce component + CSS

--- src/pages/project/ProjectClientInfo.js
+++ src/pages/project/ProjectClientInfo.js
@@ -1,21 +1,37 @@
+import './ProjectClientInfo.css'
+
 export default function ProjectClientInfo({ project }) {
   const name = project.name
   const clientName = project.clientName
   const phone = project.phone ? project.phone : '-'
   const email = project.email ? project.email : '-'
 
+  const line1 = project.address.line1? project.address.line1 : '-'
+  const line2 = project.address.line2? project.address.line2 : '-'
+  const suburb = project.address.suburb? project.address.suburb : '-'
+  const city = project.address.city? project.address.city : '-'
+  const address = line1 + ', ' + line2 + ', ' +  suburb + ', ' + city 
+  
+
   return (
     <div className="project-client-info">
-      <h2 className="page-title">Project: {name}</h2>       
-        <p>Client Name: {clientName}</p>
-        <p>Phone: {phone}</p>
-        <p>Email: {email}</p>
-        <p>Address: 
-          <span> {project.address.line1}, </span>
-          <span>{(!project.address.line2) && (!project.address.line2 === "") && `${project.address.line2},`} </span>
-          <span>{project.address.suburb}, </span>
-          <span>{project.address.city}</span>
-        </p>
+      <h2 className="page-title">Project: <strong>{name}</strong></h2>   
+      <div>
+        <ClientInfo label='Client' value={clientName}/>
+        <ClientInfo label='Phone' value={phone}/>
+        <ClientInfo label='Email' value={email}/>
+        <ClientInfo label='Name' value={clientName}/>
+        <ClientInfo label='Address' value={address}/>
+      </div>
+    </div>
+  )
+}
+
+const ClientInfo = ({label, value}) => {
+  return (
+    <div>
+      <span className='clientInfoLabel'>{label}: </span>
+      <span className='clientInfoValue'>{value}</span>
     </div>
   )
 }
